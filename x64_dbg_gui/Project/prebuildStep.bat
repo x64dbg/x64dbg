683b24f950c3151623d51f41b3ef6530ec84d407
mr.exodia <mr.exodia.tpodt@gmail.com>
2013-11-15 22:37:09 +0100

    GUI: update

--- x64_dbg_gui/Project/prebuildStep.bat
+++ x64_dbg_gui/Project/prebuildStep.bat
@@ -1,6 +1,9 @@
 @echo off
 rem Working directory: '%{sourceDir}'
 rem argument1: architecture 'x32' or 'x64'
+set result=1
 copy ..\..\bin\%1\lib%1_bridge.a Src\Bridge\
-if %ERRORLEVEL%==0 exit 0
+if %ERRORLEVEL%==0 set result=0
 copy ..\..\bin\%1\%1_bridge.lib Src\Bridge\
+if %ERRORLEVEL%==0 set result=0
+exit %result%
