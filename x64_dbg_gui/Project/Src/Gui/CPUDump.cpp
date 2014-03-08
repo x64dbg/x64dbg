6746da25c60d11a29053c7748d192e0a79d6b248
Mr. eXoDia <mr.exodia.tpodt@gmail.com>
2014-03-08 15:23:57 +0100

    GUI: hex/ascii per default

--- x64_dbg_gui/Project/Src/Gui/CPUDump.cpp
+++ x64_dbg_gui/Project/Src/Gui/CPUDump.cpp
@@ -2,47 +2,7 @@
 
 CPUDump::CPUDump(QWidget *parent) : HexDump(parent)
 {
-    int charwidth=QFontMetrics(this->font()).width(QChar(' '));
-    ColumnDescriptor_t wColDesc;
-    DataDescriptor_t dDesc;
-
-    wColDesc.isData = true; //hex byte
-    wColDesc.itemCount = 8;
-    dDesc.itemSize = Byte;
-    dDesc.byteMode = HexByte;
-    wColDesc.data = dDesc;
-    appendDescriptor(8+charwidth*23, "Hex", false, wColDesc);
-
-    wColDesc.isData = true; //ascii byte
-    wColDesc.itemCount = 8;
-    dDesc.itemSize = Byte;
-    dDesc.byteMode = AsciiByte;
-    wColDesc.data = dDesc;
-    appendDescriptor(8+charwidth*15, "ASCII", false, wColDesc);
-
-    wColDesc.isData = true; //float qword
-    wColDesc.itemCount = 1;
-    wColDesc.data.itemSize = Qword;
-    wColDesc.data.qwordMode = DoubleQword;
-    appendDescriptor(8+charwidth*23, "Double", false, wColDesc);
-
-    wColDesc.isData = true; //void*
-    wColDesc.itemCount = 1;
-#ifdef _WIN64
-    wColDesc.data.itemSize = Qword;
-    wColDesc.data.qwordMode = HexQword;
-#else
-    wColDesc.data.itemSize = Dword;
-    wColDesc.data.dwordMode = HexDword;
-#endif
-    appendDescriptor(8+charwidth*2*sizeof(uint_t), "void*", false, wColDesc);
-
-    wColDesc.isData = false; //comments
-    wColDesc.itemCount = 0;
-    dDesc.itemSize = Byte;
-    dDesc.byteMode = AsciiByte;
-    wColDesc.data = dDesc;
-    appendDescriptor(0, "Comments", false, wColDesc);
+    hexAsciiSlot();
 
     connect(Bridge::getBridge(), SIGNAL(dumpAt(int_t)), this, SLOT(printDumpAt(int_t)));
 
