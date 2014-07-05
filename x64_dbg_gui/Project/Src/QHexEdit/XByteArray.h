9424054434edcf2171f5573e4308685930e71ba8
Mr. eXoDia <mr.exodia.tpodt@gmail.com>
2014-07-05 03:01:40 +0200

    GUI: added QHexEdit class

---
+++ x64_dbg_gui/Project/Src/QHexEdit/XByteArray.h
@@ -0,0 +1,35 @@
+#ifndef XBYTEARRAY_H
+#define XBYTEARRAY_H
+
+#include <QtCore>
+
+class XByteArray
+{
+public:
+    explicit XByteArray();
+
+    QByteArray & data();
+    void setData(QByteArray data);
+    int size();
+
+    QByteArray & insert(int i, char ch);
+    QByteArray & insert(int i, const QByteArray & ba);
+
+    QByteArray & remove(int pos, int len);
+
+    QByteArray & replace(int index, char ch);
+    QByteArray & replace(int index, const QByteArray & ba);
+    QByteArray & replace(int index, int length, const QByteArray & ba);
+
+signals:
+
+public slots:
+
+private:
+    QByteArray _data; //raw byte array
+    QByteArray _mask; //masked byte array
+
+    int _oldSize;                           // size of data
+};
+
+#endif // XBYTEARRAY_H
