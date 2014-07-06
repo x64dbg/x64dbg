8bef2cef3903b8556d233c7961ff29e7e6406bdc
Mr. eXoDia <mr.exodia.tpodt@gmail.com>
2014-07-06 04:11:50 +0200

    patch list in separate thread

--- x64_dbg_bridge/bridgemain.cpp
+++ x64_dbg_bridge/bridgemain.cpp
@@ -990,9 +990,15 @@ BRIDGE_IMPEXP void GuiRepaintTableView()
     _gui_sendmessage(GUI_REPAINT_TABLE_VIEW, 0, 0);
 }
 
-BRIDGE_IMPEXP void GuiUpdatePatches()
+static DWORD WINAPI updatePatchesThread(void* a)
 {
     _gui_sendmessage(GUI_UPDATE_PATCHES, 0, 0);
+    return 0;
+}
+
+BRIDGE_IMPEXP void GuiUpdatePatches()
+{
+    CloseHandle(CreateThread(0, 0, updatePatchesThread, 0, 0, 0));
 }
 
 //Main
