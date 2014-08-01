e9249eab9775a6949b36475bd0c64ba2daf46c70
Mr. eXoDia <mr.exodia.tpodt@gmail.com>
2014-08-01 14:40:02 +0200

    EXE: updated icon id to fix the icon in the GUI

--- x64_dbg_exe/resource.h
+++ x64_dbg_exe/resource.h
@@ -2,7 +2,7 @@
 // Microsoft Visual C++ generated include file.
 // Used by resource.rc
 //
-#define IDI_ICON1                       102
+#define IDI_ICON1                       100
 
 // Next default values for new objects
 // 
