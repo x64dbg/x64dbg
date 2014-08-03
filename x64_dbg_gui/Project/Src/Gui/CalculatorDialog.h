91ee4ca292dfe5d18b2a89137c35e447f4c25de9
Mr. eXoDia <mr.exodia.tpodt@gmail.com>
2014-08-03 23:41:22 +0200

    GUI: also use the Calculator to calculate from hex, signed int, unsigned int,  octal, binary, ascii, unicode to the others

--- x64_dbg_gui/Project/Src/Gui/CalculatorDialog.h
+++ x64_dbg_gui/Project/Src/Gui/CalculatorDialog.h
@@ -15,23 +15,35 @@ class CalculatorDialog : public QDialog
     enum NUMBERFORMAT
     {
         N_HEX=16,
-        N_DEC=10,
+        N_SDEC=10,
+        N_UDEC=11,
         N_BIN=2,
         N_OCT=8,
         N_ASCII=0,
         N_UNKNOWN=-1
     };
+
 public:
     explicit CalculatorDialog(QWidget *parent = 0);
     ~CalculatorDialog();
     void setExpressionFocus();
+
 signals:
     bool validAddress(bool valid);
     void showCpu();
+
 public slots:
     void answerExpression(QString expression);
+
 private slots:
     void on_btnGoto_clicked();
+    void on_txtHex_textEdited(const QString &arg1);
+    void on_txtSignedDec_textEdited(const QString &arg1);
+    void on_txtUnsignedDec_textEdited(const QString &arg1);
+    void on_txtOct_textEdited(const QString &arg1);
+    void on_txtBin_textEdited(const QString &arg1);
+    void on_txtAscii_textEdited(const QString &arg1);
+    void on_txtUnicode_textEdited(const QString &arg1);
 
 private:
     Ui::CalculatorDialog *ui;
