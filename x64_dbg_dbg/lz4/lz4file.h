0cc9c5a0cedc89901f55ed6a28228cb8764cb119
Mr. eXoDia <mr.exodia.tpodt@gmail.com>
2014-09-11 22:06:10 +0200

    DBG: updated lz4 library

--- x64_dbg_dbg/lz4/lz4file.h
+++ x64_dbg_dbg/lz4/lz4file.h
@@ -8,7 +8,7 @@ typedef enum _LZ4_STATUS
     LZ4_FAILED_OPEN_OUTPUT,
     LZ4_NOT_ENOUGH_MEMORY,
     LZ4_INVALID_ARCHIVE,
-    LZ4_CORRUPTED_ARCHIVE
+    LZ4_CORRUPTED_ARCHIVE    
 } LZ4_STATUS;
 
 #if defined (__cplusplus)
@@ -17,7 +17,9 @@ extern "C"
 #endif
 
 __declspec(dllimport) LZ4_STATUS LZ4_compress_file(const char* input_filename, const char* output_filename);
+__declspec(dllimport) LZ4_STATUS LZ4_compress_fileW(const wchar_t* input_filename, const wchar_t* output_filename);
 __declspec(dllimport) LZ4_STATUS LZ4_decompress_file(const char* input_filename, const char* output_filename);
+__declspec(dllimport) LZ4_STATUS LZ4_decompress_fileW(const wchar_t* input_filename, const wchar_t* output_filename);
 
 #if defined (__cplusplus)
 }
