daa15f39a30250fb9c8a046e7dda1342ef7d5b49
Dither <ltrapper@gmail.com>
2014-09-23 11:22:21 +0400

    No pdb for afterbuildStep.bat

--- x64_dbg_gui/Project/afterbuildStep.bat
+++ x64_dbg_gui/Project/afterbuildStep.bat
@@ -3,4 +3,3 @@ rem Working directory: '%{sourceDir}'
 rem argument1: architecture 'x32' or 'x64'
 rem argument2: built binary directory '%{buildDir}\release' for example
 copy %2\%1_gui.dll ..\..\bin\%1\
-copy %2\%1_gui.pdb ..\..\bin\%1\
