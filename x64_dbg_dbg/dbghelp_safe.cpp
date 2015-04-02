905e94c66acf40931d5a8b7cdb9186b5eebe5ae0
Mr. eXoDia <mr.exodia.tpodt@gmail.com>
2015-04-02 13:56:37 +0200

    DBg+GUI: generic formatting

--- x64_dbg_dbg/dbghelp_safe.cpp
+++ x64_dbg_dbg/dbghelp_safe.cpp
@@ -3,64 +3,64 @@
 #include "threading.h"
 
 DWORD
-    SafeUnDecorateSymbolName(
+SafeUnDecorateSymbolName(
     __in PCSTR name,
     __out_ecount(maxStringLength) PSTR outputString,
     __in DWORD maxStringLength,
     __in DWORD flags
-    )
+)
 {
     CriticalSectionLocker locker(LockSym);
     return UnDecorateSymbolName(name, outputString, maxStringLength, flags);
 }
 BOOL
-    SafeSymUnloadModule64(
+SafeSymUnloadModule64(
     __in HANDLE hProcess,
     __in DWORD64 BaseOfDll
-    )
+)
 {
     CriticalSectionLocker locker(LockSym);
     return SymUnloadModule64(hProcess, BaseOfDll);
 }
 BOOL
-    SafeSymSetSearchPath(
+SafeSymSetSearchPath(
     __in HANDLE hProcess,
     __in_opt PCSTR SearchPath
-    )
+)
 {
     CriticalSectionLocker locker(LockSym);
     return SymSetSearchPath(hProcess, SearchPath);
 }
 DWORD
-    SafeSymSetOptions(
+SafeSymSetOptions(
     __in DWORD   SymOptions
-    )
+)
 {
     CriticalSectionLocker locker(LockSym);
     return SymSetOptions(SymOptions);
 }
 BOOL
-    SafeSymInitialize(
+SafeSymInitialize(
     __in HANDLE hProcess,
     __in_opt PCSTR UserSearchPath,
     __in BOOL fInvadeProcess
-    )
+)
 {
     CriticalSectionLocker locker(LockSym);
     return SymInitialize(hProcess, UserSearchPath, fInvadeProcess);
 }
 BOOL
-    SafeSymRegisterCallback64(
+SafeSymRegisterCallback64(
     __in HANDLE hProcess,
     __in PSYMBOL_REGISTERED_CALLBACK64 CallbackFunction,
     __in ULONG64 UserContext
-    )
+)
 {
     CriticalSectionLocker locker(LockSym);
     return SymRegisterCallback64(hProcess, CallbackFunction, UserContext);
 }
 DWORD64
-    SafeSymLoadModuleEx(
+SafeSymLoadModuleEx(
     __in HANDLE hProcess,
     __in_opt HANDLE hFile,
     __in_opt PCSTR ImageName,
@@ -69,89 +69,89 @@ DWORD64
     __in DWORD DllSize,
     __in_opt PMODLOAD_DATA Data,
     __in_opt DWORD Flags
-    )
+)
 {
     CriticalSectionLocker locker(LockSym);
     return SymLoadModuleEx(hProcess, hFile, ImageName, ModuleName, BaseOfDll, DllSize, Data, Flags);
 }
 BOOL
-    SafeSymGetModuleInfo64(
+SafeSymGetModuleInfo64(
     __in HANDLE hProcess,
     __in DWORD64 qwAddr,
     __out PIMAGEHLP_MODULE64 ModuleInfo
-    )
+)
 {
     CriticalSectionLocker locker(LockSym);
     return SymGetModuleInfo64(hProcess, qwAddr, ModuleInfo);
 }
 BOOL
-    SafeSymGetSearchPath(
+SafeSymGetSearchPath(
     __in HANDLE hProcess,
     __out_ecount(SearchPathLength) PSTR SearchPath,
     __in DWORD SearchPathLength
-    )
+)
 {
     CriticalSectionLocker locker(LockSym);
     return SymGetSearchPath(hProcess, SearchPath, SearchPathLength);
 }
 BOOL
-    SafeSymEnumSymbols(
+SafeSymEnumSymbols(
     __in HANDLE hProcess,
     __in ULONG64 BaseOfDll,
     __in_opt PCSTR Mask,
     __in PSYM_ENUMERATESYMBOLS_CALLBACK EnumSymbolsCallback,
     __in_opt PVOID UserContext
-    )
+)
 {
     CriticalSectionLocker locker(LockSym);
     return SymEnumSymbols(hProcess, BaseOfDll, Mask, EnumSymbolsCallback, UserContext);
 }
 BOOL
-    SafeSymEnumerateModules(
+SafeSymEnumerateModules(
     __in HANDLE hProcess,
     __in PSYM_ENUMMODULES_CALLBACK EnumModulesCallback,
     __in_opt PVOID UserContext
-    )
+)
 {
     CriticalSectionLocker locker(LockSym);
     return SymEnumerateModules(hProcess, EnumModulesCallback, UserContext);
 }
 BOOL
-    SafeSymGetLineFromAddr64(
+SafeSymGetLineFromAddr64(
     __in HANDLE hProcess,
     __in DWORD64 qwAddr,
     __out PDWORD pdwDisplacement,
     __out PIMAGEHLP_LINE64 Line64
-    )
+)
 {
     CriticalSectionLocker locker(LockSym);
     return SymGetLineFromAddr64(hProcess, qwAddr, pdwDisplacement, Line64);
 }
 BOOL
-    SafeSymFromName(
+SafeSymFromName(
     __in HANDLE hProcess,
     __in PCSTR Name,
     __inout PSYMBOL_INFO Symbol
-    )
+)
 {
     CriticalSectionLocker locker(LockSym);
     return SymFromName(hProcess, Name, Symbol);
 }
 BOOL
-    SafeSymFromAddr(
+SafeSymFromAddr(
     __in HANDLE hProcess,
     __in DWORD64 Address,
     __out_opt PDWORD64 Displacement,
     __inout PSYMBOL_INFO Symbol
-    )
+)
 {
     CriticalSectionLocker locker(LockSym);
     return SymFromAddr(hProcess, Address, Displacement, Symbol);
 }
 BOOL
-    SafeSymCleanup(
+SafeSymCleanup(
     __in HANDLE hProcess
-    )
+)
 {
     CriticalSectionLocker locker(LockSym);
     return SymCleanup(hProcess);
--- x64_dbg_dbg/dbghelp_safe.h
+++ x64_dbg_dbg/dbghelp_safe.h
@@ -8,40 +8,40 @@
 #endif //__GNUC__
 
 DWORD
-    SafeUnDecorateSymbolName(
+SafeUnDecorateSymbolName(
     __in PCSTR name,
     __out_ecount(maxStringLength) PSTR outputString,
     __in DWORD maxStringLength,
     __in DWORD flags
-    );
+);
 BOOL
-    SafeSymUnloadModule64(
+SafeSymUnloadModule64(
     __in HANDLE hProcess,
     __in DWORD64 BaseOfDll
-    );
+);
 BOOL
-    SafeSymSetSearchPath(
+SafeSymSetSearchPath(
     __in HANDLE hProcess,
     __in_opt PCSTR SearchPath
-    );
+);
 DWORD
-    SafeSymSetOptions(
+SafeSymSetOptions(
     __in DWORD   SymOptions
-    );
+);
 BOOL
-    SafeSymInitialize(
+SafeSymInitialize(
     __in HANDLE hProcess,
     __in_opt PCSTR UserSearchPath,
     __in BOOL fInvadeProcess
-    );
+);
 BOOL
-    SafeSymRegisterCallback64(
+SafeSymRegisterCallback64(
     __in HANDLE hProcess,
     __in PSYMBOL_REGISTERED_CALLBACK64 CallbackFunction,
     __in ULONG64 UserContext
-    );
+);
 DWORD64
-    SafeSymLoadModuleEx(
+SafeSymLoadModuleEx(
     __in HANDLE hProcess,
     __in_opt HANDLE hFile,
     __in_opt PCSTR ImageName,
@@ -50,56 +50,56 @@ DWORD64
     __in DWORD DllSize,
     __in_opt PMODLOAD_DATA Data,
     __in_opt DWORD Flags
-    );
+);
 BOOL
-    SafeSymGetModuleInfo64(
+SafeSymGetModuleInfo64(
     __in HANDLE hProcess,
     __in DWORD64 qwAddr,
     __out PIMAGEHLP_MODULE64 ModuleInfo
-    );
+);
 BOOL
-    SafeSymGetSearchPath(
+SafeSymGetSearchPath(
     __in HANDLE hProcess,
     __out_ecount(SearchPathLength) PSTR SearchPath,
     __in DWORD SearchPathLength
-    );
+);
 BOOL
-    SafeSymEnumSymbols(
+SafeSymEnumSymbols(
     __in HANDLE hProcess,
     __in ULONG64 BaseOfDll,
     __in_opt PCSTR Mask,
     __in PSYM_ENUMERATESYMBOLS_CALLBACK EnumSymbolsCallback,
     __in_opt PVOID UserContext
-    );
+);
 BOOL
-    SafeSymEnumerateModules(
+SafeSymEnumerateModules(
     __in HANDLE hProcess,
     __in PSYM_ENUMMODULES_CALLBACK EnumModulesCallback,
     __in_opt PVOID UserContext
-    );
+);
 BOOL
-    SafeSymGetLineFromAddr64(
+SafeSymGetLineFromAddr64(
     __in HANDLE hProcess,
     __in DWORD64 qwAddr,
     __out PDWORD pdwDisplacement,
     __out PIMAGEHLP_LINE64 Line64
-    );
+);
 BOOL
-    SafeSymFromName(
+SafeSymFromName(
     __in HANDLE hProcess,
     __in PCSTR Name,
     __inout PSYMBOL_INFO Symbol
-    );
+);
 BOOL
-    SafeSymFromAddr(
+SafeSymFromAddr(
     __in HANDLE hProcess,
     __in DWORD64 Address,
     __out_opt PDWORD64 Displacement,
     __inout PSYMBOL_INFO Symbol
-    );
+);
 BOOL
-    SafeSymCleanup(
+SafeSymCleanup(
     __in HANDLE hProcess
-    );
+);
 
 #endif //_DBGHELP_SAFE_H
