db6956a78d835986a90c04404af392030417e686
Mr. eXoDia <mr.exodia.tpodt@gmail.com>
2015-04-06 04:37:39 +0200

    full-featured build.bat (includes coverity builds)

--- build.bat
+++ build.bat
@@ -1,13 +1,26 @@
 @echo off
 
+echo Saving PATH
+set OLDPATH=%PATH%
+
 if "%1%"=="x32" (
-    echo Building x32
+    call setenv_x32.bat
     set type="Win32"
 ) else if "%1%"=="x64" (
-    echo Building x64
+    call setenv_x64.bat
     set type="x64"
+) else if "%1%"=="coverity" (
+    if "%2"=="" (
+        echo "usage: build.bat coverity x32/x64"
+        goto :eof
+    )
+    call setenv_coverity.bat
+    echo Building with Coverity
+    cov-configure --msvc
+    cov-build --dir cov-int --instrument build.bat %2%
+    goto :eof
 ) else (
-    echo error, invalid parameter
+    echo "usage: build.bat coverity/x32/x64"
     goto :eof
 )
 
@@ -33,3 +46,6 @@ cd x64_dbg_gui\Project
 call afterbuildStep.bat %1% ..\..\build\release
 cd ..
 cd ..
+
+echo Resetting PATH
+set PATH=%OLDPATH%
