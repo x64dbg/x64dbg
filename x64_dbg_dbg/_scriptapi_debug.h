61579618b875ec7483ac7017a2c784c2e5e7663f
Mr. eXoDia <mr.exodia.tpodt@gmail.com>
2015-08-15 20:50:12 +0200

    DBG: added software + hardware breakpoints to the scriptapi

--- x64_dbg_dbg/_scriptapi_debug.h
+++ x64_dbg_dbg/_scriptapi_debug.h
@@ -7,6 +7,13 @@ namespace Script
 {
 namespace Debug
 {
+enum HardwareType
+{
+    HardwareAccess,
+    HardwareWrite,
+    HardwareExecute
+};
+
 SCRIPT_EXPORT void Wait();
 SCRIPT_EXPORT void Run();
 SCRIPT_EXPORT void Pause();
@@ -14,6 +21,10 @@ SCRIPT_EXPORT void Stop();
 SCRIPT_EXPORT void StepIn();
 SCRIPT_EXPORT void StepOver();
 SCRIPT_EXPORT void StepOut();
+SCRIPT_EXPORT bool SetBreakpoint(duint address);
+SCRIPT_EXPORT bool DeleteBreakpoint(duint address);
+SCRIPT_EXPORT bool SetHardwareBreakpoint(duint address, HardwareType type = HardwareExecute);
+SCRIPT_EXPORT bool DeleteHardwareBreakpoint(duint address);
 }; //Debug
 }; //Script
 
