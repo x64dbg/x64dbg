378b5651f541fd6c93a404688beab80bd995b374
Torusrxxx <routeping@gmail.com>
2016-05-21 03:52:53 +0000

    GUI: Logging GUI extended
    #612 : save log
    #373 : disable log

--- src/gui/Src/Gui/LogView.h
+++ src/gui/Src/Gui/LogView.h
@@ -8,18 +8,29 @@ class LogView : public QTextEdit
     Q_OBJECT
 public:
     explicit LogView(QWidget* parent = 0);
+    void setupContextMenu();
+    void refreshShortcutsSlot();
+    void contextMenuEvent(QContextMenuEvent* event);
 
 signals:
 
 public slots:
     void updateStyle();
     void addMsgToLogSlot(QString msg);
+    void setLoggingEnabled(bool enabled);
+    bool getLoggingEnabled();
+
     void clearLogSlot();
-
+    void saveSlot();
+    void toggleLoggingSlot();
 private:
+    bool loggingEnabled;
 
-
-
+    QAction* actionCopy;
+    QAction* actionSelectAll;
+    QAction* actionClear;
+    QAction* actionSave;
+    QAction* actionToggleLogging;
 };
 
 #endif // LOGVIEW_H
