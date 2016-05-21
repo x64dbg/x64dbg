5ffe5b14a9fd1f78f2efd7ab7fd0bc7b56fde7ce
Torusrxxx <routeping@gmail.com>
2016-05-21 04:08:08 +0000

    #613 : copy all registers

--- src/gui/Src/Gui/RegistersView.h
+++ src/gui/Src/Gui/RegistersView.h
@@ -137,6 +137,7 @@ protected slots:
     void onToggleValueAction();
     void onCopyToClipboardAction();
     void onCopySymbolToClipboardAction();
+    void onCopyAllAction();
     void onFollowInDisassembly();
     void onFollowInDump();
     void onFollowInStack();
@@ -211,6 +212,7 @@ private:
     QAction* wCM_ToggleValue;
     QAction* wCM_CopyToClipboard;
     QAction* wCM_CopySymbolToClipboard;
+    QAction* wCM_CopyAll;
     QAction* wCM_FollowInDisassembly;
     QAction* wCM_FollowInDump;
     QAction* wCM_FollowInStack;
