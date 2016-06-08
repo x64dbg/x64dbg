1638d15e76dba0bdf810e8c1f1969e53c8ac59e7
Kamil <machimabx@gmail.com>
2016-06-08 21:11:34 +0200

    test commit

--- src/gui/Src/Exports.h
+++ src/gui/Src/Exports.h
@@ -5,13 +5,19 @@
 
 /************************************************************************************
                             Exported Function Prototypes
+
 ************************************************************************************/
+
+
+
 
 #ifdef BUILD_LIB
 extern "C" __declspec(dllexport) int _gui_guiinit(int argc, char* argv[]);
 extern "C" __declspec(dllexport) void* _gui_sendmessage(GUIMSG type, void* param1, void* param2);
 extern "C" __declspec(dllexport) void _gui_sendmessageasync(GUIMSG type, void* param1, void* param2);
 #endif
+
+
 
 
 #endif // EXPORTS_H
