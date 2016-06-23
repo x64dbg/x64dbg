0ef378d521e5a44356243ab57be2f53b982b252d
Torusrxxx <routeping@gmail.com>
2016-06-23 13:35:15 +0000

    translate launcher

---
+++ src/exe/LoadResourceString.h
@@ -0,0 +1,18 @@
+#include <string>
+
+#include "resource1.h" // String resources
+
+inline std::wstring _LoadResString(UINT uID) {
+    wchar_t* p = nullptr;
+    int len = ::LoadStringW(NULL, uID, reinterpret_cast<LPWSTR>(&p), 0);
+    if(len > 0)
+    {
+        return std::wstring(p, len);
+    }
+    else
+    {
+        return std::wstring();
+    }
+}
+
+#define LoadResString(uID) _LoadResString(uID).c_str()
