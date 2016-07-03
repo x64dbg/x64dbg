e2f04153f041b2f0c85bfe33e51aa9ba5e6195f1
Torusrxxx <routeping@gmail.com>
2016-07-03 08:22:00 +0000

    Add simple message box

--- src/gui/Src/Utils/MiscUtil.h
+++ src/gui/Src/Utils/MiscUtil.h
@@ -5,5 +5,8 @@
 
 void SetApplicationIcon(WId winId);
 QByteArray & ByteReverse(QByteArray & array);
+QString SimpleInputBox(QWidget* parent, const QString & title, QString defaultValue = "");
+void SimpleErrorBox(QWidget* parent, const QString & title, const QString & text);
+void SimpleWarningBox(QWidget* parent, const QString & title, const QString & text);
 
 #endif // MISCUTIL_H
