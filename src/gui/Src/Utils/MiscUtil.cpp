07e22027305bb8392f0a1029c142a9f2cc50d747
Torusrxxx <routeping@gmail.com>
2016-07-03 08:23:10 +0000

    Add simple message box

--- src/gui/Src/Utils/MiscUtil.cpp
+++ src/gui/Src/Utils/MiscUtil.cpp
@@ -1,5 +1,8 @@
 #include "MiscUtil.h"
 #include <windows.h>
+#include "LineEditDialog.h"
+#include <QMessageBox>
+#include <QIcon>
 
 void SetApplicationIcon(WId winId)
 {
@@ -18,4 +21,35 @@ QByteArray & ByteReverse(QByteArray & array)
         array[length - i - 1] = temp;
     }
     return array;
+}
+
+QString SimpleInputBox(QWidget* parent, const QString & title, QString defaultValue)
+{
+    LineEditDialog mEdit(parent);
+    mEdit.setWindowIcon(parent->windowIcon());
+    mEdit.setText(defaultValue);
+    mEdit.setWindowTitle(title);
+    mEdit.setCheckBox(false);
+    if(mEdit.exec() == QDialog::Accepted)
+        return mEdit.editText;
+    else
+        return "";
+}
+
+void SimpleErrorBox(QWidget* parent, const QString & title, const QString & text)
+{
+    QMessageBox msg(QMessageBox::Critical, title, text, QMessageBox::NoButton, parent);
+    msg.setWindowIcon(QIcon(":/icons/images/compile-error.png"));
+    msg.setParent(parent, Qt::Dialog);
+    msg.setWindowFlags(msg.windowFlags() & (~Qt::WindowContextHelpButtonHint));
+    msg.exec();
+}
+
+void SimpleWarningBox(QWidget* parent, const QString & title, const QString & text)
+{
+    QMessageBox msg(QMessageBox::Warning, title, text, QMessageBox::NoButton, parent);
+    msg.setWindowIcon(QIcon(":/icons/images/compile-warning.png"));
+    msg.setParent(parent, Qt::Dialog);
+    msg.setWindowFlags(msg.windowFlags() & (~Qt::WindowContextHelpButtonHint));
+    msg.exec();
 }
