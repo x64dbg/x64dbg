4a0a8cec4cc506e553e2afc2cfee26ae00785f41
Torusrxxx <routeping@gmail.com>
2016-07-06 14:38:31 +0000

    favourite tools

---
+++ src/gui/Src/Gui/FavouriteTools.h
@@ -0,0 +1,49 @@
+#ifndef FAVOURITETOOLS_H
+#define FAVOURITETOOLS_H
+
+#include <QDialog>
+#include <QTableWidget>
+
+namespace Ui {
+class FavouriteTools;
+}
+
+class FavouriteTools : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit FavouriteTools(QWidget *parent = 0);
+    ~FavouriteTools();
+public slots:
+    void on_btnAddFavouriteTool_clicked();
+    void on_btnRemoveFavouriteTool_clicked();
+    void on_btnDescriptionFavouriteTool_clicked();
+    void on_btnUpFavouriteTool_clicked();
+    void on_btnDownFavouriteTool_clicked();
+    void on_btnAddFavouriteScript_clicked();
+    void on_btnRemoveFavouriteScript_clicked();
+    void on_btnDescriptionFavouriteScript_clicked();
+    void on_btnUpFavouriteScript_clicked();
+    void on_btnDownFavouriteScript_clicked();
+    void on_btnAddFavouriteCommand_clicked();
+    void on_btnRemoveFavouriteCommand_clicked();
+    void on_btnUpFavouriteCommand_clicked();
+    void on_btnDownFavouriteCommand_clicked();
+    void onListSelectionChanged();
+    void on_shortcutEdit_askForSave();
+    void on_btnOK_clicked();
+private:
+    Ui::FavouriteTools *ui;
+    QKeySequence currentShortcut;
+    int originalToolsCount;
+    int originalScriptCount;
+    int originalCommandCount;
+
+    void upbutton(QTableWidget* table);
+    void downbutton(QTableWidget* table);
+
+    void setupTools(QString name, QTableWidget* list);
+};
+
+#endif // FAVOURITETOOLS_H
