e8bbbc90e746ad34f6ce3d7bd5a04abac70bf4b1
torusrxxx <torusrxxx@users.noreply.github.com>
2016-12-11 15:45:50 +0800

    Find window in attach dialog

--- src/gui/Src/Gui/AttachDialog.cpp
+++ src/gui/Src/Gui/AttachDialog.cpp
@@ -2,6 +2,7 @@
 #include "ui_AttachDialog.h"
 #include "SearchListView.h"
 #include <QMenu>
+#include <QMessageBox>
 
 AttachDialog::AttachDialog(QWidget* parent) : QDialog(parent), ui(new Ui::AttachDialog)
 {
@@ -83,6 +84,42 @@ void AttachDialog::on_btnAttach_clicked()
         pid.sprintf("%.8X", pid.toULong());
     DbgCmdExec(QString("attach " + pid).toUtf8().constData());
     accept();
+}
+
+void AttachDialog::on_btnFindWindow_clicked()
+{
+    QString windowText;
+retryFindWindow:
+    if(!SimpleInputBox(this, tr("Find Window"), windowText, windowText, tr("Enter window title or class name here.")))
+        return;
+    HWND hWndFound = FindWindowW(NULL, reinterpret_cast<LPCWSTR>(windowText.utf16())); //Window Title first
+    if(hWndFound == NULL)
+        hWndFound = FindWindowW(reinterpret_cast<LPCWSTR>(windowText.utf16()), NULL); //Then try window class name
+    if(hWndFound == NULL)
+    {
+        QMessageBox retryDialog(QMessageBox::Critical, tr("Find Window"), tr("Cannot find window \"%1\". Retry?").arg(windowText), QMessageBox::Cancel | QMessageBox::Retry, this);
+        retryDialog.setWindowIcon(DIcon("compile-error.png"));
+        if(retryDialog.exec() == QMessageBox::Retry)
+            goto retryFindWindow;
+    }
+    else
+    {
+        DWORD pid, tid;
+        if(tid = GetWindowThreadProcessId(hWndFound, &pid))
+        {
+            refresh();
+            windowText = QString().sprintf(ConfigBool("Gui", "PidInHex") ? "%.8X" : "%u", pid);
+            for(int i = 0; i < mSearchListView->mList->getRowCount(); i++)
+            {
+                if(mSearchListView->mList->getCellContent(i, 0) == windowText)
+                {
+                    mSearchListView->mList->setSingleSelection(i);
+                }
+            }
+        }
+        else
+            SimpleErrorBox(this, tr("Find Window"), tr("GetWindowThreadProcessId() failed. Cannot get the PID of the window."));
+    }
 }
 
 void AttachDialog::processListContextMenu(QMenu* wMenu)
