0ab8b66d8508f77568830adfc70b0eeae6e315c5
torusrxxx <torusrxxx@users.noreply.github.com>
2017-04-10 22:05:18 +0800

    Improved user experience on single-core CPU

--- src/dbg/animate.cpp
+++ src/dbg/animate.cpp
@@ -1,5 +1,6 @@
 #include "animate.h"
 #include "x64dbg.h"
+#include "command.h"
 
 static char animate_command[deflen];
 static unsigned int animate_interval = 50;
@@ -10,9 +11,14 @@ static DWORD WINAPI animateThread(void* arg1)
     while(animate_command[0] != 0)
     {
         auto beforeTime = GetTickCount();
-        if(!DbgCmdExecDirect(animate_command))
+        if(!cmddirectexec(animate_command))
+        {
             // An error occurs
-            break;
+            duint value = 0;
+            BridgeSettingGetUint("Misc", "AnimateStopWhenError", &value);
+            if(value != 0)
+                break;
+        }
         auto currentTime = GetTickCount();
         if(currentTime < (beforeTime + animate_interval))
         {
