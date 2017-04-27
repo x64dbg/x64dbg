a3506502f2b418e6a090f9360a9787d827102ad5
Torusrxxx <torusrxxx@users.noreply.github.com>
2017-04-27 11:52:31 +0000

    fixed #1566 (#1567)

--- src/gui/Src/Gui/SymbolView.cpp
+++ src/gui/Src/Gui/SymbolView.cpp
@@ -477,7 +477,7 @@ void SymbolView::moduleBrowse()
     char szModPath[MAX_PATH] = "";
     if(DbgFunctions()->ModPathFromAddr(modbase, szModPath, _countof(szModPath)))
     {
-        QProcess::startDetached("explorer.exe", QStringList(QString("/select,").append(QString(szModPath))));
+        QProcess::startDetached("explorer.exe", QStringList({QString("/select,"), QString(szModPath)}));
     }
 }
 
