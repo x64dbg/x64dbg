dce0e11713bccf9d18ad7e6d519f24e1e6e64f00
torusrxxx <torusrxxx@users.noreply.github.com>
2018-05-02 12:41:40 +0800

    data copy working

--- src/gui/Src/Gui/HexEditDialog.h
+++ src/gui/Src/Gui/HexEditDialog.h
@@ -44,6 +44,61 @@ private:
 
     QByteArray resizeData(QByteArray & data);
     bool checkDataRepresentable(int mode); //1=ASCII, 2=Unicode, 3=User-selected codepage, 4=String editor, others(0)=All modes
+
+    //The following code is from Data Copy Dialog
+private slots:
+    void on_listType_currentRowChanged(int currentRow);
+    void on_buttonCopy_clicked();
+    void on_spinBox_valueChanged(int arg1);
+
+private:
+    int mIndex;
+
+    enum DataType
+    {
+        DataCByte = 0,
+        DataCWord,
+        DataCDword,
+        DataCQword,
+        DataCString,
+        DataCUnicodeString,
+        DataCShellcodeString,
+        DataASMByte,
+        DataASMWord,
+        DataASMDWord,
+        DataASMQWord,
+        DataASMString,
+        DataPascalByte,
+        DataPascalWord,
+        DataPascalDword,
+        DataPascalQword,
+        DataString,
+        DataUnicodeString,
+        DataUTF8String,
+        DataUCS4String,
+        DataHexStream,
+        DataGUID,
+        DataIPv4,
+        DataIPv6,
+        DataBase64,
+        DataMD5,
+        DataSHA1,
+        DataSHA256,
+        DataSHA512,
+        DataSHA256_3,
+        DataSHA512_3,
+        DataLast
+    };
+
+    struct FormatType
+    {
+        QString name;
+        int itemsPerLine;
+    };
+
+    FormatType mTypes[DataLast];
+
+    void printData(DataType type);
 };
 
 #endif // HEXEDITDIALOG_H
