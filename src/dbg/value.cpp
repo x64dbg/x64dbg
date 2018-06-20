ae9bf8c49ee1733ec51c0fc184ce112e2140bf70
torusrxxx <torusrxxx@users.noreply.github.com>
2018-06-20 17:00:35 +0800

    st(X) can be edited

--- src/dbg/value.cpp
+++ src/dbg/value.cpp
@@ -2028,6 +2028,7 @@ static bool startsWith(const char* pre, const char* str)
 #define XMM_PRE_FIELD_STRING "XMM"
 #define YMM_PRE_FIELD_STRING "YMM"
 #define x8780BITFPU_PRE_FIELD_STRING "x87r"
+#define x8780BITFPU_PRE_FIELD_STRING_ST "st"
 #define STRLEN_USING_SIZEOF(string) (sizeof(string) - 1)
 
 /**
@@ -2208,6 +2209,56 @@ static void setfpuvalue(const char* string, duint value)
             found = false;
             break;
         }
+        if(found)
+            SetContextDataEx(hActiveThread, registerindex, value);
+    }
+    else if(startsWith(x8780BITFPU_PRE_FIELD_STRING_ST, string))
+    {
+        flags = GetContextDataEx(hActiveThread, UE_X87_STATUSWORD);
+        flags >>= 11;
+        flags &= 7;
+        string += STRLEN_USING_SIZEOF(x8780BITFPU_PRE_FIELD_STRING_ST);
+        bool found = true;
+        DWORD registerindex;
+        switch(*string)
+        {
+        case '0':
+            registerindex = flags;
+            break;
+
+        case '1':
+            registerindex = ((1 + flags) & 7);
+            break;
+
+        case '2':
+            registerindex = ((2 + flags) & 7);
+            break;
+
+        case '3':
+            registerindex = ((3 + flags) & 7);
+            break;
+
+        case '4':
+            registerindex = ((4 + flags) & 7);
+            break;
+
+        case '5':
+            registerindex = ((5 + flags) & 7);
+            break;
+
+        case '6':
+            registerindex = ((6 + flags) & 7);
+            break;
+
+        case '7':
+            registerindex = ((7 + flags) & 7);
+            break;
+
+        default:
+            found = false;
+            break;
+        }
+        registerindex += UE_x87_r0;
         if(found)
             SetContextDataEx(hActiveThread, registerindex, value);
     }
