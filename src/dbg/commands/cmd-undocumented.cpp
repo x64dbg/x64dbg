584ae50818eca4c9c0430a35477f59e57595f7fd
Duncan Ogilvie <mr.exodia.tpodt@gmail.com>
2018-07-01 19:02:36 +0200

    DBG: add information to Zydis command

--- src/dbg/commands/cmd-undocumented.cpp
+++ src/dbg/commands/cmd-undocumented.cpp
@@ -240,6 +240,10 @@ bool cbInstrZydis(int argc, char* argv[])
     auto instr = cp.GetInstr();
     int argcount = instr->operandCount;
     dputs_untranslated(cp.InstructionText(true).c_str());
+    dprintf_untranslated("prefix size: %d\n", instr->raw.prefixes.count);
+    dprintf_untranslated("disp.offset: %d, disp.size: %d\n", instr->raw.disp.offset, instr->raw.disp.size);
+    dprintf_untranslated("imm[0].offset: %d, imm[0].size: %d\n", instr->raw.imm[0].offset, instr->raw.imm[0].size);
+    dprintf_untranslated("imm[1].offset: %d, imm[1].size: %d\n", instr->raw.imm[1].offset, instr->raw.imm[1].size);
     dprintf_untranslated("size: %d, id: %d, opcount: %d\n", cp.Size(), cp.GetId(), instr->operandCount);
     auto rwstr = [](uint8_t access)
     {
