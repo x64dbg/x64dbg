x64dbg/x64dbg

commit 52af639cc4df6e0b90d84d2188e44ebf80732d44

Enhanced LogView performance

src/gui/Src/Gui/LogView.cpp
@@ -266,7 +266,7 @@ void LogView::addMsgToLogSlot(QByteArray msg)
     if(redirectError)
         msgUtf16.append(tr("fwrite() failed (GetLastError()= %1 ). Log redirection stopped.\n").arg(GetLastError()));
 
-    if(logBuffer.length() >= 1024 * 1024 * 100) // 100mb buffer limit
+    if(logBuffer.length() >= MAX_LOG_BUFFER_SIZE)
         logBuffer.clear();
 
     logBuffer.append(msgUtf16);
@@ -434,13 +434,16 @@ void LogView::flushTimerSlot()
     counter--;
     if(counter == 0)
     {
-        if(document()->characterCount() > 1024 * 1024 * 100) //limit the log to ~100mb
+        if(document()->characterCount() > MAX_LOG_BUFFER_SIZE)
             clear();
         counter = 100;
     }
-    QTextCursor cursor = textCursor();
+    QTextCursor cursor(document());
     cursor.movePosition(QTextCursor::End);
+    cursor.beginEditBlock();
+    cursor.insertBlock();
     cursor.insertHtml(logBuffer);
+    cursor.endEditBlock();
     if(autoScroll)
         moveCursor(QTextCursor::End);
     setUpdatesEnabled(true);

src/gui/Src/Gui/LogView.h
@@ -35,6 +35,8 @@ public slots:
     void flushLogSlot();
 
 private:
+    static const int MAX_LOG_BUFFER_SIZE = 1024 * 1024;
+
     bool loggingEnabled;
     bool autoScroll;
     bool utf16Redirect = false;

commit 8577c58f680cd4d800f7d2416689d05f34596551

GUI: fix small bug introduced by #2243

src/gui/Src/Gui/LogView.cpp
@@ -442,6 +442,8 @@ void LogView::flushTimerSlot()
     cursor.movePosition(QTextCursor::End);
     cursor.beginEditBlock();
     cursor.insertBlock();
+    // hack to not insert too many newlines: https://lists.qt-project.org/pipermail/qt-interest-old/2011-July/034725.html
+    cursor.deletePreviousChar();
     cursor.insertHtml(logBuffer);
     cursor.endEditBlock();
     if(autoScroll)