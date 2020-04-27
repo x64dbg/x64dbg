9d49c67a755dd3365ebabf4565bd9285a37165df
Duncan Ogilvie <mr.exodia.tpodt@gmail.com>
2020-04-28 00:32:21 +0200

    PROJECT: update release.bat to include the new loaddll.exe

--- release.bat
+++ release.bat
@@ -72,10 +72,12 @@ copy bin\x32\x32bridge.dll %RELEASEDIR%\release\x32\
 copy bin\x32\x32dbg.dll %RELEASEDIR%\release\x32\
 copy bin\x32\x32dbg.exe %RELEASEDIR%\release\x32\
 copy bin\x32\x32gui.dll %RELEASEDIR%\release\x32\
+copy bin\x32\loaddll.exe %RELEASEDIR%\release\x32\
 copy bin\x64\x64bridge.dll %RELEASEDIR%\release\x64\
 copy bin\x64\x64dbg.dll %RELEASEDIR%\release\x64\
 copy bin\x64\x64dbg.exe %RELEASEDIR%\release\x64\
 copy bin\x64\x64gui.dll %RELEASEDIR%\release\x64\
+copy bin\x64\loaddll.exe %RELEASEDIR%\release\x64\
 
 echo "creating commithash.txt"
 git rev-parse HEAD > %RELEASEDIR%\commithash.txt
