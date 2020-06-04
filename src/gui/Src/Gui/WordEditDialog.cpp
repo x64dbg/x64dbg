59f06a203734221c7e8125d16e199f5460555852
JustasMasiulis <justas.masiulis@yahoo.com>
2020-06-04 22:42:24 +0300

    minor forced element styling removals

--- src/gui/Src/Gui/WordEditDialog.cpp
+++ src/gui/Src/Gui/WordEditDialog.cpp
@@ -78,9 +78,6 @@ void WordEditDialog::expressionChanged(bool validExpression, bool validPointer, 
     Q_UNUSED(validPointer);
     if(validExpression)
     {
-        ui->expressionLineEdit->setStyleSheet("");
-        ui->signedLineEdit->setStyleSheet("");
-        ui->unsignedLineEdit->setStyleSheet("");
         ui->btnOk->setEnabled(true);
 
         //hex
