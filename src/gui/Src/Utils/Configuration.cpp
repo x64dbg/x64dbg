01989be2fbaa53361e3a00deb83b1b1f09cf1cd2
torusrxxx <torusrxxx@users.noreply.github.com>
2020-08-07 12:04:55 +0800

    Highlight user modules

--- src/gui/Src/Utils/Configuration.cpp
+++ src/gui/Src/Utils/Configuration.cpp
@@ -239,6 +239,8 @@ Configuration::Configuration() : QObject(), noMoreMsgbox(false)
     defaultColors.insert("BreakpointSummaryKeywordColor", QColor("#8B671F"));
     defaultColors.insert("BreakpointSummaryStringColor", QColor("#008000"));
     defaultColors.insert("PatchRelocatedByteHighlightColor", QColor("#0000DD"));
+    defaultColors.insert("SymbolUserTextColor", QColor("#FF0000"));
+    defaultColors.insert("SymbolSystemTextColor", QColor("#000000"));
     defaultColors.insert("SymbolUnloadedTextColor", QColor("#000000"));
     defaultColors.insert("SymbolLoadingTextColor", QColor("#8B671F"));
     defaultColors.insert("SymbolLoadedTextColor", QColor("#008000"));
