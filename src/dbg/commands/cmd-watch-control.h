c44b869b69787b64468647741eae19dc5dd993f1
morsisko <morsisko@gmail.com>
2020-10-22 01:24:53 +0200

    DBG: Added new command SetWatchType

--- src/dbg/commands/cmd-watch-control.h
+++ src/dbg/commands/cmd-watch-control.h
@@ -7,4 +7,5 @@ bool cbDelWatch(int argc, char* argv[]);
 bool cbSetWatchdog(int argc, char* argv[]);
 bool cbSetWatchExpression(int argc, char* argv[]);
 bool cbSetWatchName(int argc, char* argv[]);
+bool cbSetWatchType(int argc, char* argv[]);
 bool cbCheckWatchdog(int argc, char* argv[]);
