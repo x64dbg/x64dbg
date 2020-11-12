c69ab584fe9e39ac1382b4342086ea42c5bf6ba9
Duncan Ogilvie <mr.exodia.tpodt@gmail.com>
2020-11-12 03:31:37 +0100

    Fix compilation for 32-bit

--- src/dbg/module.cpp
+++ src/dbg/module.cpp
@@ -1231,6 +1231,7 @@ bool ModRelocationsInRange(duint Address, duint Size, std::vector<MODRELOCATIONI
     return !Relocations.empty();
 }
 
+#if _WIN64
 const RUNTIME_FUNCTION* MODINFO::findRuntimeFunction(DWORD rva) const
 {
     const auto found = std::lower_bound(runtimeFunctions.cbegin(), runtimeFunctions.cend(), rva, [](const RUNTIME_FUNCTION & a, const DWORD & rva)
@@ -1243,6 +1244,7 @@ const RUNTIME_FUNCTION* MODINFO::findRuntimeFunction(DWORD rva) const
 
     return nullptr;
 }
+#endif
 
 bool MODINFO::loadSymbols(const String & pdbPath, bool forceLoad)
 {
