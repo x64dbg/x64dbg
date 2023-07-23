8d182a977e15287cefd974ed33bf6b828577e29e
dad <dad@mail.com>
2023-07-23 17:02:57 -0600

    Update References tab highlighting
    Increments or decrements the highlighted reference in References tab when using Actions->Go to Next/Previous Reference

--- src/dbg/exprfunc.cpp
+++ src/dbg/exprfunc.cpp
@@ -518,6 +518,7 @@ namespace Exprfunc
     duint refsearchaddr(duint row)
     {
         auto content = GuiReferenceSearchGetCellContent(int(row), 0);
+        GuiReferenceSetSingleSelection(int(row), false);
         duint addr = 0;
         valfromstring(content, &addr, false);
         BridgeFree(content);
