e11aabca495e8bdafd28380a3a0681198d6d6060
VisualPlugin <aboshadi.n.ahmed@gmail.com>
2025-10-23 16:03:33 -0700

    Update expressionfunctions.cpp

--- src/dbg/expressionfunctions.cpp
+++ src/dbg/expressionfunctions.cpp
@@ -72,7 +72,7 @@ void ExpressionFunctions::Init()
     RegisterEasy("mod.main,mod.mainbase", dbgdebuggedbase);
     RegisterEasy("mod.rva", modrva);
     RegisterEasy("mod.offset,mod.fileoffset", valvatofileoffset);
-    RegisterEasy("mod.fromoffset,mod.fromfileoffset", valfileoffsettova);
+    ExpressionFunctions::Register("mod.fromoffset,mod.fromfileoffset", ValueTypeNumber, { ValueTypeString, ValueTypeNumber }, valfileoffsettova);
     RegisterEasy("mod.headerva", modheaderva);
     RegisterEasy("mod.isexport", modisexport);
     ExpressionFunctions::Register("mod.fromname", ValueTypeNumber, { ValueTypeString }, Exprfunc::modbasefromname);
@@ -303,4 +303,5 @@ bool ExpressionFunctions::isValidName(const String & name)
             return false;
     return true;
 }
+
 
