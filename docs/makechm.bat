c3b46ab0c3a3e31beb2afde5990921b923fb907c
aHkr <ahkrichards@gmail.com>
2026-02-08 09:26:59 -0500

    Qualifying batch path for files to resolve successfully during build

--- docs/makechm.bat
+++ docs/makechm.bat
@@ -22,13 +22,13 @@ if not exist "%~dp0faqs" (
 	git submodule update --init
 	git submodule update --remote --recursive
 )
-cd faqs
-git apply ..\wiki.patch
-cd ..
+cd /d "%~dp0faqs"
+git apply "%~dp0\wiki.patch"
+cd /d "%~dp0"
 
 echo Building Help Project
 echo Generating FAQ shim
-call "%PORTABLE_PYTHON%\python.exe" .\md_to_rst_faq.py --out-dir "faqs"
+call "%PORTABLE_PYTHON%\python.exe" "%~dp0\md_to_rst_faq.py" --out-dir "faqs"
 if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 call make htmlhelp
 if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
