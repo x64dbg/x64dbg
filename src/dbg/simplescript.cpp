0ddda0bfe064cd43e10f9f188e79767df31abc29
OldGamesCracking <OldGamesCracking@gmail.com>
2026-03-22 15:01:37 +0100

    Format

--- src/dbg/simplescript.cpp
+++ src/dbg/simplescript.cpp
@@ -176,7 +176,7 @@ static bool scriptCreateLineMap(const char* filename, bool gui)
             int add = 0;
             while(isspace(temp[add]))
                 add++;
-            if (add < 254)
+            if(add < 254)
             {
                 strcpy_s(entry.raw, temp + add);
                 scriptLineMap.push_back(entry);
