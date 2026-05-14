66819a118cff674fabbb7839dd583c16a3f089a4
VisualPlugin <aboshadi.n.ahmed@gmail.com>
2026-05-14 07:57:54 +0000

    Update assemble.h

--- src/dbg/assemble.h
+++ src/dbg/assemble.h
@@ -13,6 +13,6 @@ enum class AssemblerEngine
 extern AssemblerEngine assemblerEngine;
 
 bool assemble(duint addr, unsigned char* dest, int* size, const char* instruction, char* error);
-bool assembleat(duint addr, const char* instruction, int* size, char* error, bool fillnop);
+bool assembleat(duint addr, const char* instruction, int* size, char* error, bool fillnop, bool split = false);
 
 #endif // _ASSEMBLE_H
